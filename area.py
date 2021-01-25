84b84f32defb81f5708e5397822d1d1dc3503080
KaranGiri121 <karangiri121@gmail.com>
2021-01-25 21:22:37 +0530

    Area Class Error Get Fixed

--- area.py
+++ area.py
@@ -1,38 +1,52 @@
-class area:
-    def ans(self,user_input,user_area):
-        getattr(self,user_input,lambda e : print('Valid Entry'))()
-    def sqrtdecimeter(self,user_area):
-        sqrtmeter=sqrtdecimeter/100
-        return sqrtmeter
-    def sqrtcentimeter(self,user_area):
-        sqrtmeter=user_area/10000
-        return sqrtmeter
-    def sqrtkilometer(self,user_area):
-        sqrtmeter=user_area*1000000
-        return sqrtmeter
-    def sqrtmilimeter(self,user_area):
-        sqrtmeter=user_area/1000000
-        return sqrtmeter
-    def are(self,user_area):
-        sqrtmeter=user_area*100
-        return sqrtmeter
-    def hectare(self,user_area):
-        sqrtmeter=user_area*10000
-        return sqrtmeter
-    def to_sqrtdecimeter(self,user_area):
-        return user_area*100
-    def to_sqrtcentimeter(self,user_area):
-        return user_area*10000
-    def to_sqrtkilometer(self,user_area):
-        return user_area/1000000
-    def to_sqrtmilimeter(self,user_area):
-        return user_area*1000000
-    def to_are(self,user_area):
-        return user_area/100
-    def to_hectare(self,user_area):
-        return user_area/10000
-    def sqrtmeter(self,user_area):
-        return user_area
+class Area:
+	def ans(self,user_input,user_area):
+		dict={'Square Decimeter':'sqrtdecimeter','Square Milimeter':'sqrtmilimeter','Square Centimeter':'sqrtcentimeter','Square Kilometer':'sqrtkilometer','Square Meter':'sqrtmeter'}
+		if user_input in dict:
+			user_input=dict[user_input]
+		return getattr(self,user_input,lambda e : print('Valid Entry'))(user_area)
+	def sqrtdecimeter(self,user_area):
+		sqrtmeter=user_area/100
+#		print(sqrtmeter)
+		return sqrtmeter
+	def sqrtcentimeter(self,user_area):
+		sqrtmeter=user_area/10000
+		return sqrtmeter
+	def sqrtkilometer(self,user_area):
+		sqrtmeter=user_area*1000000
+		return sqrtmeter
+	def sqrtmilimeter(self,user_area):
+		sqrtmeter=user_area/1000000
+		return sqrtmeter
+	def are(self,user_area):
+		sqrtmeter=user_area*100
+		return sqrtmeter
+	def hectare(self,user_area):
+		sqrtmeter=user_area*10000
+		return sqrtmeter
+	def to_sqrtdecimeter(self,user_area):
+		answer=user_area*100
+		print('Hello World')
+		return answer
+	def to_sqrtcentimeter(self,user_area):
+		answer=user_area*10000
+		return answer
+	def to_sqrtkilometer(self,user_area):
+		answer=user_area/1000000
+		return answer
+	def to_sqrtmilimeter(self,user_area):
+		answer=user_area*1000000
+		return answer
+	def to_are(self,user_area):
+		answer=user_area/100
+		return answer
+	def to_hectare(self,user_area):
+		answer=user_area/10000
+		return answer
+	def sqrtmeter(self,user_area):
+		answer=user_area
+		return answer
+	def to_sqrtmeter(self,user_area):
+		return user_area
 
 
 
--- unitconverter.py
+++ unitconverter.py
@@ -1,7 +1,8 @@
 import os
 from length import Len
+from area import Area
 def menu():
-#	os.system('clear')
+	dict={1:Lenconversion,2:Areaconversion}
 	print('''Welcome To Unit Converter
 	[1]Length
 	[2]Area
@@ -16,11 +17,9 @@ Enter Your Choice: ''')
 	except Exception:
 		print('Enter Valid Value')
 		menu()
-	if user_menu==1:
-		user_menu_1()
-	elif user_menu==5:
-		quit()
-def user_menu_1():
+	dict.get(user_menu,default)()
+
+def Lenconversion():
 	while True:
 		os.system('clear')
 		print("Length Conversion :") 
@@ -31,8 +30,8 @@ def user_menu_1():
 			user_input=int(input('Enter The Unit Conversion From : ')) 
 			user_length=int(input(f'{len_menu[user_input]}: '))
 		except Exception:
-			print("Enter Valid Number/Choice")
-			user_menu_1()
+			print('Enter Valid Entry')
+			Lenconversion()
 		a=Len()
 		meter=a.ans(len_menu[user_input],user_length)
 		for i in len_menu:
@@ -45,7 +44,41 @@ def user_menu_1():
 		user_input=input('Press Y For Unitconversion : ')
 		if user_input!='y' and user_input!='Y':
 			menu()
-
+def Areaconversion():
+	while True:
+		os.system('clear')
+		print('Area Conversion')
+		area_menu={1:'Square Decimeter',
+			2:'Square Milimeter',
+			3:'Square Centimeter',
+			4:'Square Meter',
+			5:'Square Kilometer',
+			6:'Are',
+			7:'Hectare'}
+		for i in area_menu:
+			print(f'{i}:{area_menu[i]}')
+		try:
+			user_input=int(input("Enter Unit Conversion From :"))
+			user_area=int(input(f'{area_menu[user_input]}: '))
+		except Exception:
+			print("Enter Valid Entry")
+			Areaconversion()
+		a=Area()
+		sqrtmeter=a.ans(area_menu[user_input],user_area)
+		for i in area_menu:
+			print(f'{i}:{area_menu[i]} ')
+		user_input=int(input('Enter Unit Coversion To : '))
+		to_area={1:'to_sqrtdecimeter',2:'to_sqrtmilimeter',3:'to_sqrtcentimeter',4:'to_sqrtmeter',
+			5:'to_sqrtkilometer',6:'to_are',7:'to_hectare'}
+		print(sqrtmeter,to_area[user_input])
+		answer=a.ans(to_area[user_input],sqrtmeter)
+		print(f'{area_menu[user_input]}: {answer}')
+		print('Press Any Key To Back To Menu Or',end='\n')
+		user_input=input('Press Y For Unitconversion : ')
+		if user_input!='y' and user_input!='Y':
+			menu()
+def default():
+	quit()
 
 
 
